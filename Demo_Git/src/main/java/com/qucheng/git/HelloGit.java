c4036e259de04c31f33a9ef7e28bec33825de37f
QuchengRep1 <QuchengRep_1@163.com>
2021-01-04 14:29:13 +0800

    Master 分支更新 1

--- Demo_Git/src/main/java/com/qucheng/git/HelloGit.java
+++ Demo_Git/src/main/java/com/qucheng/git/HelloGit.java
@@ -4,5 +4,6 @@ package com.qucheng.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git ...");
+        System.out.println("Master 分支更新 1...");
     }
 }
